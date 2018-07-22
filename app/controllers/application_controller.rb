e5631bd403fc921dae9a7e55ea055a5c61354697
Shea Kusiak <shkusiak@gmail.com>
2018-07-22 17:49:55 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,24 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :super_hero
+    end
+
+    post '/teams' do
+      # binding.pry
+      @team_name = params[:team][:name]
+      @team_motto = params[:team][:motto]
+      @heros = []
+      @heros_powers = []
+      @heros_bios = []
+      @team_members = params[:team][:members]
+      @team_members.each do |hero|
+        @heros << hero[:name]
+        @hero_powers << hero[:power]
+        @hero_bios << hero[:bio]
+      end
+      erb :team
+    end
 
 end
